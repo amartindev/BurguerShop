b329105c9428887d71a4f5d5890bdd51bb4c19f4
Antonio Martin <luis.martinf89@gmail.com>
2023-03-14 14:47:57 -0500

    creacion banner en capas y carruseles independientes

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,66 @@
-var swiper = new Swiper(".slider-content", {
+var swiper1 = new Swiper("#swiper1", {
     slidesPerView: 3,
     spaceBetween: 30,
     loop: true,
     centerSlide: 'true',
     fade: 'true',
     grabCursor: 'true',
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        }
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: true,
+      },
+  });
+
+var swiper2 = new Swiper("#swiper2", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        }
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: true,
+      },
+  });
+
+  var swiper3 = new Swiper("#swiper3", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
